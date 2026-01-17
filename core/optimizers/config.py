ff3a994f523b96f89e12d6c5f356e2f3a21bc868
Nadine <nadine@Nadines-MacBook-Pro.local>
2026-01-17 15:39:07 -0500

    OptimizerConfig implemented

--- core/optimizers/config.py
+++ core/optimizers/config.py
@@ -1,6 +1,6 @@
 import copy
 from abc import ABC, abstractmethod
-from typing import Optional, Union
+from typing import Optional, Union, Type, Self
 
 from core.optimizers.base import Optimizer
 from core.utils import WorldType
@@ -9,14 +9,14 @@ from core.utils import WorldType
 class _Config(ABC):
     def to_dict(self) -> dict:
         """Converts this configuration to dict format."""
+        raise NotImplementedError
 
 
-# TODO this has to be immutable
-# TODO optimizer registry
+class OptimizerConfig(_Config, ABC):
 
-
-class OptimizerConfig(_Config):
-    def __init__(self, opt_class: Optional[type] = None):
+    # TODO registry to allow opt_class str
+    # TODO runtime checking of opt_class
+    def __init__(self, opt_class: Optional[Type[Optimizer]] = None):
         """Initializes an OptimizerConfig instance.
 
         Args:
@@ -29,11 +29,57 @@ class OptimizerConfig(_Config):
         # World specs
         self._world = None
 
+        # Has this config object been frozen (cannot alter its attributes anymore).
+        self._is_frozen = False
+
+        # Evaluation
+        self.evaluation_config : Optional["OptimizerConfig"] = None
+
+    def __setattr__(self, name, value):
+        if hasattr(self, "_is_frozen") and self._is_frozen:
+            if name not in ["_is_frozen"]:
+                raise AttributeError(
+                    f"Cannot set attribute ({name}) of an already frozen "
+                    "OptimizerConfig!"
+                )
+        super().__setattr__(name, value)
+
+    def copy(self, copy_frozen: Optional[bool] = None) -> Self:
+        """Creates a deep copy of this config and (un)freezes if necessary.
+
+        Args:
+            copy_frozen: Whether the created deep copy is frozen or not, If None,
+                keep the same frozen status that 'self' currently has.
+
+        Returns:
+            A deep copy of 'self' that is (un)frozen.
+        """
+        cp = copy.deepcopy(self)
+        if copy_frozen is True:
+            cp.freeze()
+        elif copy_frozen is False:
+            cp._is_frozen = False
+            if isinstance(cp.evaluation_config, OptimizerConfig):
+                cp.evaluation_config._is_frozen = False
+        return cp
+    
+    # TODO freezing for nested configs
+    def freeze(self) -> None:
+        """Freeze this config object, such that no attributes can be set anymore.
+
+        Optimizers should use this method to make sure their config objects 
+        remain read-only after this.
+        """
+        if self._is_frozen:
+            return
+        self._is_frozen = True
+
+
+    # TODO deep copy allows on may be toggled later with use_copy
+    # TODO build_optimizer() to accept logger_creator: Optional[Callable[[], Logger]] = None,
     def build_optimizer(
         self,
         world: Optional[Union[str, WorldType]] = None,
-        # logger_creator: Optional[Callable[[], Logger]] = None,
-        use_copy: bool = True,
     ) -> Optimizer:
         """Builds an Optimizer from this OptimizerConfig (or a copy thereof).
 
@@ -42,23 +88,18 @@ class OptimizerConfig(_Config):
             logger_creator: Callable that creates a logger object. If unspecified, a default logger is created.
 
         """
+        cfg = self.copy() 
         if world is not None:
-            self.world = world
+            cfg._world = world
+        cfg.freeze() # attention this would freeze the cfg even if the world is None !
 
-        # if logger_creator is not None:
-        #         self.logger_creator = logger_creator
-
-        if isinstance(self.opt_class, str):
-            opt_class = get_trainable_cls(self.opt_class)
-
-        return opt_class(
-            config=self if not use_copy else copy.deepcopy(self),
-            # logger_creator=self.logger_creator,
-        )
+        opt_class = self.opt_class
+        
+        return opt_class(config=cfg)
 
     # TODO Docstring explanation
     @abstractmethod
-    def world(self, world=Optional[Union[str, WorldType]]):
+    def world(self, world: Optional[Union[str, WorldType]]):
         """
         Docstring for world
 
