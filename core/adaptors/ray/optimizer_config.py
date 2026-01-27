6eb60e2b12b0112ad99447f77d51e0c7e9135b54
Nadine <nadine@Nadines-MacBook-Pro.local>
2026-01-26 21:22:41 -0500

    feat: enhance RayOptimizerConfig to support local environment creation and registration

--- core/adaptors/ray/optimizer_config.py
+++ core/adaptors/ray/optimizer_config.py
@@ -1,11 +1,12 @@
+import uuid
 from typing import Optional, Self
 
 from ray.rllib.algorithms.algorithm import Algorithm
 from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
+from ray.tune.registry import register_env
 
 from core.adaptors.ray.optimizer import RayOptimizer
 from core.annotations import override
-from core.envs.base import BaseEnv
 from core.optimizers.base import Optimizer
 from core.optimizers.config import OptimizerConfig
 from core.world.base import World
@@ -21,10 +22,15 @@ class RayOptimizerConfig(OptimizerConfig):
             raise ValueError(f"{self.__class__.__name__} must define `algo_class`")
         super().__init__(opt_class=RayOptimizer)
 
+        # TODO termporary setting until find out how to share world context accross runners
         self.ray_cfg: AlgorithmConfig = (
             self.algo_class.get_default_config()
             .environment(None)
-            .env_runners(num_env_runners=0)
+            .env_runners(
+                num_env_runners=0,
+                create_local_env_runner=True,
+                create_env_on_local_worker=True,
+            )
         )
 
     def validate(self) -> None:
@@ -98,11 +104,29 @@ class RayOptimizerConfig(OptimizerConfig):
         return self
 
     @override(OptimizerConfig)
-    def build_optimizer(self, *, world: Optional[World] = None, inner_opt: Optional[Optimizer] = None, **kwargs):
+    def build_optimizer(
+        self,
+        *,
+        world: Optional[World] = None,
+        inner_opt: Optional[Optimizer] = None,
+        **kwargs,
+    ):
         cfg = self.copy(copy_frozen=True)
         if cfg.opt_class is None:
             raise ValueError("OptimizerConfig has no opt_class")
-        
+
+        env_name = f"regulated_env_{uuid.uuid4().hex}"
+
+        def env_creator(env_ctx):
+            return cfg._env_creator(
+                world=world,
+                inner_opt=inner_opt,
+                **cfg.env_config,
+            )
+
+        register_env(env_name, env_creator)
+        self.ray_cfg = self.ray_cfg.environment(env=env_name)
+
         algo = self.ray_cfg.build(**kwargs)
         opt = RayOptimizer(algo=algo, config=cfg)
 
@@ -111,11 +135,7 @@ class RayOptimizerConfig(OptimizerConfig):
             opt_id = world.register_optimizer(opt)
             opt.set_id(opt_id)
 
-        env = cfg._env_creator(world=world, inner_opt=inner_opt)
-        env.set_opt_id(opt.id)
-        opt.env = env
-        
-        return 
+        return opt
 
     @override(OptimizerConfig)
     def freeze(self) -> None:
