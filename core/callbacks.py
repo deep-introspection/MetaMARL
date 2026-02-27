cc0ca7803d4418769609266a70a36312f9b32540
Nadine <nadine@Nadines-MacBook-Pro.local>
2026-02-27 16:05:20 -0500

    feat: add function to tag episodes with environment index in callbacks

---
+++ core/callbacks.py
@@ -0,0 +1,6 @@
+from ray.rllib.env.multi_agent_episode import MultiAgentEpisode
+# from ray.rllib.env.vector.vector_multi_agent_env import VectorMultiAgentEnv
+
+def tag_episode_with_env_idx(*, episode: MultiAgentEpisode, env_index: int, **kwargs):
+    episode_id = episode.id_
+    episode.id_ = f"{env_index}|{episode_id}"
